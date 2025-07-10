361b7cc863be75428520074288a53fbc78eebdcc
Jyoti Singh <jyotisinghd106@gmail.com>
2025-07-10 23:48:18 +0530

    Update main.jsx

--- flavorhut-app/src/main.jsx
+++ flavorhut-app/src/main.jsx
@@ -1,14 +1,14 @@
-// src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css'; // Import your Tailwind CSS base styles
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrap App with BrowserRouter */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
