c609d799f9c5409bdf8d3144991c59cf3a99548b
Jyoti Singh <jyotisinghd106@gmail.com>
2025-07-10 22:40:35 +0530

    Update server.js

--- flavorhut-2 backend/server.js
+++ flavorhut-2 backend/server.js
@@ -7,7 +7,13 @@ console.log('Current working directory:', process.cwd());
 const fs = require('fs');
 
 const PORT = process.env.PORT || 5000;
-
+app.get("/", (req,res) => {
+  try{
+      res.send("This API is working properly");
+  }catch(err){
+      res.send("Internal server Error");
+  }
+})
 connectDB();
 
 app.listen(PORT, () => {
